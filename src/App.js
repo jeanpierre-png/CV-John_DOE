9c949c0671f6dfc366f0f6b1c6a39859b28057fa
NOZA Jean-Pierre <nozajean-pierre@MacBook-Air-de-Jean-Pierre.local>
2024-05-18 09:57:39 -0300

    Modification du pied de page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, } from "react-router-dom";
+import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
@@ -10,9 +11,38 @@ import MentionsLegales from "./pages/MentionsLegales";
 import Github from "./pages/Github";
 import Footer from "./components/Footer";
 import "./stylesCss/styles.css";
+import arrow_top from "../src/assets/image/fleche-incurvee.png";
 
 
 export default function App() {
+    const [visible, visibles] = useState(false);
+
+    useEffect(() => {
+
+        function scroll() {
+            if (window.scrollY > 300) {
+                visibles(true);
+            } else {
+                visibles(false);
+            }
+        }
+
+        window.addEventListener('scroll', scroll);
+
+        return () => { 
+            window.removeEventListener('scroll', scroll);
+        };
+
+    }, []);
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+
     return (
 
         <div className="App">
@@ -24,17 +54,21 @@ export default function App() {
                 <Route path="/réalisations" element={<Realisations />}></Route>
                 <Route path="/blog" element={<Blog />}></Route>
                 <Route path="/contacter" element={<Contact />}></Route>
-                <Route path="/mentions-legales" element={<MentionsLegales/>}></Route>
-                <Route path="/github-john-doe" element={<Github/>}></Route>
+                <Route path="/mentions-legales" element={<MentionsLegales />}></Route>
+                <Route path="/github-john-doe" element={<Github />}></Route>
             </Routes>
 
-           <Footer/>
-           
-           
+            <footer>
+                <Footer />
+            </footer>
 
-        </div> 
-        
-        
+            {visible && (
+                <button onClick={scrollToTop} className="scroll-btn">
+                    <img className="arrow" src={arrow_top} alt= "flèche retour vers le haut de la page"/>
+                </button>
+            )}
+
+        </div>
 
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,15 +14,18 @@ export default function Footer() {
 
     return (
         <>
-            <div className="background d-flex flex-column flex-md-row flex-lg-row justify-content-lg-center p-5" style={{ backgroundColor: "#EEE" }}>
-                <div className="col-md-3">
-                    <h3 className='fs-4'>John Doe</h3>
-                    <address>
-                        40 rue Laure Diebold <br />
-                        69009 Lyon, France <br />
-                        Téléphone : 06 20 30 40 50
+            <div className="background d-flex flex-column p-3 flex-md-row flex-md-wrap p-md-5 flex-lg-row justify-content-lg-center" style={{ backgroundColor: "#EEE" }}>
+                <div className="col-md-6 col-lg-3">
+                    <h3 className='fs-5'>John Doe</h3>
+                    <address className="col-7 col-md-6 m-0">
+                        <a className="bloc-footer" href="https://maps.app.goo.gl/nL94idcaVvr5Q24u6" target="_blank">
+                        40 rue Laure Diebold
+                        69009 Lyon, France</a>
                     </address>
-                    <div className='d-flex gap-4'>
+
+                    <a className="bloc-footer col-md-12" href="tel:0620304050">Téléphone : 06 20 30 40 50</a>  
+
+                    <div className='d-flex gap-4 mt-3 mb-1 m-md-0 mt-md-4'>
                         <Link to="/github-john-doe" rel="noreferrer"><img className="img" src={github} alt="github" /></Link>
 
                         <a className="text-dark" target="_blank" rel="noreferrer" href="http://twitter.com/"><img className="img" src={twitter} alt="twitter" /></a>
@@ -30,32 +33,32 @@ export default function Footer() {
                     </div>
                 </div>
 
-                <div className="col-md-3 gap-sm-5 ">
+                <div className="gap-sm-5 mb-2 mb-md-1 col-md-6 col-lg-3">
                     <h3 className='fs-5'>Liens utiles</h3>
-                    <div className="">
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/">Accueil</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/#a-propos">À propos</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/services">Services</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/contacter">Me contacter</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/mentions-legales">Mentions Légales</Link></div>
+                    <div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/">Accueil</Link></div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/#a-propos">À propos</Link></div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/services">Services</Link></div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/contacter">Me contacter</Link></div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/mentions-legales">Mentions Légales</Link></div>
                     </div>
                 </div>
 
-                <div className="col-md-3">
+                <div className="col-md-6 col-lg-3">
                     <h3 className='fs-5'>Mes dernières réalisations</h3>
-                    <div >
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/réalisations">Fresh food</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/réalisations">Restaurant Akira</Link></div>
-                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/réalisations">Espace bien-être</Link></div>
+                    <div>
+                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/réalisations">Fresh food</Link></div>
+                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/réalisations">Restaurant Akira</Link></div>
+                        <div><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/réalisations">Espace bien-être</Link></div>
                     </div>
                 </div>
 
-                <div className="col-md-3">
+                <div className="mt-2 col-md-6 col-lg-3 m-lg-0 ">
                     <h3 className='fs-5'>Mes derniers articles</h3>
                     <div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/blog">Coder son site en HTML/CSS</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/blog">Vendre ses produits sur le web</Link></div>
-                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link to="/blog">Se positionner sur Google</Link></div>
+                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/blog">Coder son site en HTML/CSS</Link></div>
+                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/blog">Vendre ses produits sur le web</Link></div>
+                        <div ><img className="img" src={angle_droit} alt="fleche-droite" /><Link className="bloc-footer" to="/blog">Se positionner sur Google</Link></div>
                     </div>
                 </div>
             </div>
